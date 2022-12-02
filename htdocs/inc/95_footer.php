24e2a6ce1b9c1ec5529ad8a3b2205e549690822f
Liksagen.com <nmetauo@gmail.com>
2022-12-02 23:19:14 +0000

    sync logo and footer with main

--- htdocs/inc/95_footer.php
+++ htdocs/inc/95_footer.php
@@ -1,154 +1,101 @@
+	<!-- Footer -->
+	<footer id="footer" class="bg-gray-900 text-white">
 
-			<!-- Footer -->
-			<footer id="footer" class="bg-gray-900 text-white">
+		<!-- footer cta -->
+		<div class="container tet-white py-5 d-lg-flex flex-row align-items-lg-center">
 
-				<!-- footer cta -->
-				<div class="container tet-white py-5 d-lg-flex flex-row align-items-lg-center">
+			<div class="row g-4 align-items-center">
+				<h3 class="col-lg-6 h2 mb-0">
+					Let your capital work for you today in the most efficient way!
+				</h3>
+				<div class="col text-lg-end">
+					<a href="https://dimple.finance" target="_blank" rel="noopener" class="btn btn-light rounded-pill d-inline-flex align-items-center px-4">
+						<span class="text-dark me-2">Go to Main</span>
+						<svg width="18px" height="18px" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+							<path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"></path>
+						</svg>
+					</a>
+				</div>
+			</div>
 
-					<div class="row g-4 align-items-center">
-						<h3 class="col-lg-6 h2 mb-0">
-							Join our community by using our services and grow your business.
-						</h3>
-						<div class="col text-lg-end">
-							<a href="https://wrapbootstrap.com/theme/smarty-website-admin-rtl-WB02DSN1B" target="_blank" rel="noopener" class="btn btn-warning rounded-pill d-inline-flex align-items-center px-4">
-								<span class="text-dark me-2">Start now</span>
-								<svg width="18px" height="18px" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">  
-								  <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"></path>
-								</svg>
-							</a>
-						</div>
+		</div>
+		<!-- /footer cta -->
+
+		<!-- line divider -->
+		<hr class="bg-gray-500 my-0 container">
+
+		<div class="container py-5">
+
+			<div class="row g-4">
+
+				<div class="col-12 col-lg-4 mb-4">
+
+					<!-- logo -->
+<span class="footer-logo">
+					<img width="110" height="38" src="assets/images/logo/logo_light.svg" alt="...">
+</span>
+
+					<div class="my-4"><!-- copyright -->
+						<p class="small mb-0">&copy; 2022 Dimple Finance.</p>
+						<p class="small mb-0">All rights reserved.</p>
 					</div>
 
+					<!-- social icons -->
+					<!-- twitter -->
+					<a href="https://twitter.com/Dimple_Finance" target="_blank" class="btn btn-sm text-white transition-hover-top mb-2 rounded-circle bg-gray-700" rel="noopener" aria-label="twitter page">
+						<i class="fi fi-social-twitter"></i>
+					</a>
+
+					<!-- github -->
+					<a href="https://github.com/DimpleFinance" target="_blank" class="btn btn-sm text-white transition-hover-top mb-2 rounded-circle bg-gray-700" rel="noopener" aria-label="twitter page">
+						<i class="fi fi-social-github"></i>
+					</a>
+
 				</div>
-				<!-- /footer cta -->
 
-				<!-- line divider -->
-				<hr class="bg-gray-500 my-0 container">
-				
-				<div class="container py-5">
+				<div class="col-12 col-lg-4 mb-4">
 
-					<div class="row g-4">
-						
-						<div class="col-12 col-lg-4 mb-4">
+					<h3 class="small mb-4">GET IN TOUCH</h3>
 
-							<!-- logo -->
-<span class="footer-logo">Dimple</span>
-<!--							<img width="110" height="38" src="assets/images/logo/logo_light.svg" alt="...">-->
+					<p class="small col-md-10">
+						If you have any questions or suggestions please feel free to contact us by email.
+					</p>
 
-							<div class="my-4"><!-- copyright -->
-								<p class="small mb-0">&copy; 2022 All rights reserved</p>
-<!--								<p class="small mb-0">All rights reserved.</p>-->
-							</div>
-
-							<!-- social icons -->
-							<a href="#" class="btn btn-sm text-white transition-hover-top mb-2 rounded-circle bg-gray-700" rel="noopener" aria-label="facebook page">
-								<i class="fi fi-social-facebook"></i> 
-							</a>
-
-							<a href="#" class="btn btn-sm text-white transition-hover-top mb-2 rounded-circle bg-gray-700" rel="noopener" aria-label="twitter page">
-								<i class="fi fi-social-twitter"></i> 
-							</a>
-
-							<a href="#" class="btn btn-sm text-white transition-hover-top mb-2 rounded-circle bg-gray-700" rel="noopener" aria-label="linkedin page">
-								<i class="fi fi-social-linkedin"></i> 
-							</a>
-
-							<a href="#" class="btn btn-sm text-white transition-hover-top mb-2 rounded-circle bg-gray-700" rel="noopener" aria-label="youtube page">
-								<i class="fi fi-social-youtube"></i> 
-							</a>
-
-						</div>
-
-						<div class="col-12 col-lg-4 mb-4">
-
-							<h3 class="small mb-4">GET IN TOUCH</h3>
-
-							<p class="small col-md-10">
-								Smarty is a responsive multipurpose template, created to help you build new projects in no time! Clean, fast, simple!
-							</p>
-
-	            <div class="small d-flex align-items-start my-3">
-	              <svg class="me-3 flex-none my-1" height="24px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-	                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-	                <circle cx="12" cy="10" r="3"></circle>
-	              </svg>
-	              <ul class="list-unstyled p-0 m-0 w-100">
-	                <li>PO Box 21132</li>
-	                <li>Here Weare St, Melbourne</li>
-	                <li>Vivas 2355 Australia</li>
-	              </ul>
-	            </div>
-
-	            <div class="small d-flex align-items-start my-3">
-	              <svg class="me-3 flex-none my-1" height="24px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">  
-	                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path>
-	              </svg>
-	              <ul class="list-unstyled p-0 m-0 w-100">
-	                <li>Phone: <a href="tel:18005652390" class="text-white text-dashed">1-800-565-2390</a></li>
-	                <li>Email: <a href="mail:myemail@gmail.com" class="text-white text-dashed">myemail@gmail.com</a></li>
-	              </ul>
-	            </div>
-
-						</div>
-
-
-						<div class="col-12 col-lg-4 mb-4">
-
-							<h3 class="small mb-4">LEARN MORE</h3>
-
-							<ul class="list-unstyled"> 
-								<li class="list-item mb-2">
-									<i class="fi fi-arrow-end me-2 smaller"></i>
-									<a class="text-gray-400" href="contact-1.html">Contact</a>
-								</li> 
-								<li class="list-item mb-2">
-									<i class="fi fi-arrow-end me-2 smaller"></i>
-									<a class="text-gray-400" href="about-us-1.html">About Us</a>
-								</li> 
-								<li class="list-item mb-2">
-									<i class="fi fi-arrow-end me-2 smaller"></i>
-									<a class="text-gray-400" href="page-terms-and-conditions.html">Terms & Conditions</a>
-								</li> 
-								<li class="list-item mb-2">
-									<i class="fi fi-arrow-end me-2 smaller"></i>
-									<a class="text-gray-400" href="page-cookie.html">GDPR & Cookies</a>
-								</li> 
-							</ul>
-
-						</div>
-
+					<div class="small d-flex align-items-start my-3">
+						<svg class="me-3 flex-none my-1" height="24px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path>
+						</svg>
+						<ul class="list-unstyled p-0 m-0 w-100">
+							<li>Email: <a href="mail:hello@dimple.finance" class="text-white text-dashed">hello@dimple.finance</a></li>
+						</ul>
 					</div>
 
 				</div>
 
-			</footer>
-			<!-- /Footer -->
 
+				<div class="col-12 col-lg-4 mb-4">
 
-		</div><!-- /#wrapper -->
-    <script type="text/javascript" src="/js-web3/web3.js"></script>
-    <script type="text/javascript" src="/js-web3/web3modal.js"></script>
-    <script type="text/javascript" src="/js-web3/evm-chains.js"></script>
+					<h3 class="small mb-4">LEARN MORE</h3>
 
-    <script type="text/javascript" src="/js-web3/wallet-connect.js"></script>
-    <script type="text/javascript" src="/js-web3/ethers-5.0.umd.min.js"></script>
-    <script type="text/javascript" src="/js-web3/bignumber.min.js"></script>
+					<ul class="list-unstyled">
+						<li class="list-item mb-2">
+							<p class="text-gray-400">Cooming soon</p>
+						</li>
+						<!--						<li class="list-item mb-2">-->
+						<!--							<i class="fi fi-arrow-end me-2 smaller"></i>-->
+						<!--							<a class="text-gray-400" href="page-terms-and-conditions.html">Terms & Conditions</a>-->
+						<!--						</li>-->
+						<!--						<li class="list-item mb-2">-->
+						<!--							<i class="fi fi-arrow-end me-2 smaller"></i>-->
+						<!--							<a class="text-gray-400" href="page-cookie.html">GDPR & Cookies</a>-->
+						<!--						</li>-->
+					</ul>
 
+				</div>
 
-<?php
-print "<script>";
-print "
-var glob = new Array();
-glob[\"api_url\"] = \"$api\";
-glob[\"api2_url\"] = \"$api2\";
-glob[\"site_version\"] = '$site_version';
-glob[\"title\"] = 'DIMPLE';
-";
-print "</script>";
-?>
-		<script src="/js2/"></script>
-		<script src="assets/js/core.min.js"></script>
-		<script type="text/javascript" src="/js/simple02.js?007"></script>
+			</div>
 
-	</body>
-</html>
+		</div>
+
+	</footer>
+	<!-- /Footer -->
